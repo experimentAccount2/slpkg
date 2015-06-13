b1b37780a731d857cf35c32f5b83d4222f546549
Dimitris Zlatanidis <d.zlatanidis@gmail.com>
2015-06-13 16:34:40 +0300

    Fix check if path exist

--- slpkg/pkg/find.py
+++ slpkg/pkg/find.py
@@ -34,8 +34,9 @@ def find_package(find_pkg, directory):
     """
     pkgs = []
     blacklist = BlackList().packages()
-    for pkg in sorted(os.listdir(directory)):
-        if (not pkg.startswith(".") and pkg.startswith(find_pkg) and
-                split_package(pkg)[0] not in blacklist):
-            pkgs.append(pkg)
+    if os.path.exists(directory):
+        for pkg in sorted(os.listdir(directory)):
+            if (not pkg.startswith(".") and pkg.startswith(find_pkg) and
+                    split_package(pkg)[0] not in blacklist):
+                pkgs.append(pkg)
     return pkgs
