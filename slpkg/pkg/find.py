8ac2a7ba41a7c484fc71b1e449d5978fa4e39859
Dimitris Zlatanidis <d.zlatanidis@gmail.com>
2015-07-26 06:47:26 +0300

    Fix user interrupt

--- slpkg/pkg/find.py
+++ slpkg/pkg/find.py
@@ -32,12 +32,16 @@ def find_package(find_pkg, directory):
     Find packages
     """
     pkgs = []
-    if os.path.isdir(directory):
-        installed = sorted(os.listdir(directory))
-        blacklist = BlackList().packages(pkgs=installed, repo="local")
-        if os.path.exists(directory):
-            for pkg in installed:
-                if (not pkg.startswith(".") and pkg.startswith(find_pkg) and
-                        split_package(pkg)[0] not in blacklist):
-                    pkgs.append(pkg)
+    try:
+        if os.path.isdir(directory):
+            installed = sorted(os.listdir(directory))
+            blacklist = BlackList().packages(pkgs=installed, repo="local")
+            if os.path.exists(directory):
+                for pkg in installed:
+                    if (not pkg.startswith(".") and pkg.startswith(find_pkg) and
+                            split_package(pkg)[0] not in blacklist):
+                        pkgs.append(pkg)
+    except KeyboardInterrupt:
+        print("")
+        raise SystemExit()
     return pkgs
