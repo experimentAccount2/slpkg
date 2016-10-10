28147fb5d83efde1fd6a34e2280c7474db1bfa14
Dimitris Zlatanidis <d.zlatanidis@gmail.com>
2016-10-10 15:49:53 +0300

    Update help and usage exit message

--- slpkg/arguments.py
+++ slpkg/arguments.py
@@ -49,85 +49,113 @@ Usage: slpkg [COMMANDS|OPTIONS] {repository|package...}
 Commands:
    update, --only=[...]                      Run this command to update all
                                              the packages list.
+
    upgrade, --only=[...]                     Delete and recreate all packages
                                              lists.
+
    repo-add [repository name] [URL]          Add custom repository.
+
    repo-remove [repository]                  Remove custom repository.
+
    repo-enable                               Enable or disable default
                                              repositories via dialog utility.
+
    repo-list                                 Print a list of all the
                                              repositories.
+
    repo-info [repository]                    Get information about a
                                              repository.
+
    update slpkg                              Upgrade the program directly from
                                              repository.
+
    health, --silent                          Health check installed packages.
+
    deps-status, --tree, --graph=[type]       Print dependencies status used by
                                              packages or drawing dependencies
                                              diagram.
+
    new-config                                Manage .new configuration files.
 
 Optional arguments:
   -h | --help                                Print this help message and exit.
+
   -v | --version                             Print program version and exit.
+
   -a | --autobuild, [script] [source...]     Auto build SBo packages.
                                              If you already have downloaded the
                                              script and the source code you can
                                              build a new package with this
                                              command.
+
   -b | --blacklist, [package...] --add,      Manage packages in the blacklist.
        --remove, list                        Add or remove packages and print
                                              the list. Each package is added
                                              here will not be accessible by the
                                              program.
+
   -q | --queue, [package...] --add,          Manage SBo packages in the queue.
        --remove, list, build, install,       Add or remove and print the list
        build-install                         of packages. Build and then
                                              install the packages from the
                                              queue.
+
   -g | --config, print, edit, reset          Configuration file management.
                                              Print, edit the configuration file
                                              or reset in the default values.
+
   -l | --list, [repository], --index,        Print a list of all available
        --installed, --name                   packages from repository, index or
                                              print only packages installed on
                                              the system.
+
   -c | --check, [repository], --upgrade,     Check for updated packages from
        --skip=[...], --resolve--off          the repositories and upgrade or
        --checklist                           install with all dependencies.
+
   -s | --sync, [repository] [package...],    Sync packages. Install packages
        --resolve-off, --download-only,       directly from remote repositories
-       --directory-prefix=[dir], --case-ins  with all dependencies.
+       --directory-prefix=[dir],             with all dependencies.
+       --case-ins
+
   -t | --tracking, [repository] [package],   Tracking package dependencies and
        --check-deps, --graph=[type],         print package dependencies tree
        --case-ins                            with highlight if packages is
                                              installed. Also check if
                                              dependencies used or drawing
                                              dependencies diagram.
+
   -p | --desc, [repository] [package],       Print description of a package
        --color=[]                            directly from the repository and
                                              change color text.
+
   -n | --network, [package], --checklist,    View a standard of SBo page in
        --case-ins                            terminal and manage multiple
                                              options like reading, downloading,
                                              building, installation, etc.
+
   -F | --FIND, [package...], --case-ins      Find packages from each enabled
                                              repository and view results.
+
   -f | --find, [package...], --case-ins      Find and print installed packages
                                              reporting the size and the sum.
+
   -i | --installpkg, [options] [package...]  Installs single or multiple *.tgz
        options=[--warn, --md5sum, --root,    (or .tbz, .tlz, .txz) Slackware
        --infobox, --menu, --terse, --ask,    binary packages designed for use
        --priority, --tagfile]                with the Slackware Linux
                                              distribution onto your system.
+
   -u | --upgradepkg, [options] [package...]  Upgrade single or multiple
        options=[--dry-run, --install-new,    Slackware binary packages from
        --reinstall, --verbose]               an older version to a newer one.
+
   -r | --removepkg, [options] [package...],  Removes a previously installed
        --deps, --check-deps, --tag,          Slackware binary packages,
        --checklist                           while writing a progress report
        options=[-warn, -preserve, -copy,     to the standard output.
        -keep]                                Use only package name.
+
   -d | --display, [package...]               Display the contents of installed
                                              packages and file list.
 
